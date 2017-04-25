551d63193519b807c9452347544a3ee6d3a4150e
Silvio Fernandes <silvio.fernandes@resultadosdigitais.com.br>
2017-04-25 14:23:19 -0300

    .

--- lib/pmf_nfpse_generator.rb
+++ lib/pmf_nfpse_generator.rb
@@ -46,10 +46,10 @@ class PmfNfpseGenerator
 
   class Configuration
     attr_accessor :TipoSistema, :Emissor_Identificacao, :Emissor_AEDF, :Emissor_TipoAedf, :Emissor_Cidade, :Emissor_Estado, :Impostos
-    # I18n.config.enforce_available_locales = false
-    # I18n.load_path = Dir['../config/locales/*.yml']
-    # I18n.locale = 'pt-BR'
-    # I18n.default_locale = 'pt-BR'
+    I18n.config.enforce_available_locales = false
+    I18n.load_path = Dir['../config/locales/*.yml']
+    I18n.locale = 'pt-BR'
+    I18n.default_locale = 'pt-BR'
   end
 
   # {"city"=>"Curitiba", "state"=>"PR", "city_ibge_code"=>"4106902", "source"=>"csv"}
